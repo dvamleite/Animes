afc72ff17ff7ec3afa6e9177d5c5116ef2a2c6a3
Dvam leite <dvanlds@gmail.com>
2022-03-10 12:36:01 -0300

    fist-comit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+j
+import axios from 'axios'
+import { useState } from 'react/cjs/react.production.min'
+
 export default function Home() {
+  const [anime,setAnimes] = useState({})
+
+  useEffect(() =>{
+    fetch(`https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=0`)
+    .then((response) => response.json())
+    .then((response) =>{        
+        setAnimes(response);                  
+    })
+}, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,44 +26,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+       <h1>{anime.attributes.cannonicalTitle}</h1>
+    
       </main>
 
       <footer className={styles.footer}>
