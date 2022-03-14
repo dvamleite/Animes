07f6e62ab7ac1edc99001c6ac09d4ad67b213336
Dvam leite <dvanlds@gmail.com>
2022-03-14 14:55:06 -0300

    style

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
 import Menu from './components/menu'
 import Search from './components/search'
 import Link from 'next/link'
@@ -58,20 +56,6 @@ export default function Home() {
                    )}   
       
         </main>
-
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <span className={styles.logo}>
-              <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-            </span>
-          </a>
-        </footer>     
-  
   </>   
   )
 }
